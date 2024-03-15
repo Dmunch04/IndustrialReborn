6402fae54d0972f5a1d316dea8530acd93b7f6e0
Daniel Munch <daniellmunch@gmail.com>
2024-03-15 13:11:05 +0100

    fixed todo

--- src/main/java/me/munchii/industrialreborn/IndustrialReborn.java
+++ src/main/java/me/munchii/industrialreborn/IndustrialReborn.java
@@ -10,13 +10,11 @@ import reborncore.common.config.Configuration;
 
 public class IndustrialReborn implements ModInitializer {
     // TODO: make new animation for powered spawner because the current looks cursed
-    // TODO: make textures for mob slaughter, soul extractor and fluid transposer
-    // TODO: AnimalFeeder block texture
-    // TODO: AnimalBabySeparator block texture
-
-    // TODO: redesign gui for above machines maybe? (probably not fluid transposer)
+    // TODO: make textures for mob slaughter, soul extractor, fluid transposer, animal feeder and animal baby separator
+    // TODO: balance machines a bit out tier wise (max energy, max input, energy consumption)
 
     // TODO: AnimalRancher block
+    // TODO: Sewer block? (how to use the fertilizer? when greenhouse controller already exists in TR)
 
     public static final String MOD_ID = "industrialreborn";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
